2d03281f729fc1ccbbcb7104af8e21b2c0357947
patrickcavalcante <patrickc.lima@outlook.com>
2021-01-07 14:04:01 -0300

    fist commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,47 @@
+import { GetServerSideProps } from 'next';
+import { Title } from '../styles/pages/Home';
+import SEO from '../components/SEO';
+
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: IProduct[];
+}
+
+export default function Home({ recommendedProducts }: HomeProps ) {
+  return (
+    <div>
+      <SEO
+       title="DevCommerce, your best e-commerce!"
+       image="boost.png"
+       shouldExcludeTitleSuffix
+      />
+      <section>
+        <Title>Products</Title>
+        <ul>
+          {recommendedProducts.map(recommendedProduct => {
+            return (
+              <li key={recommendedProduct.id}>
+                {recommendedProduct.title}
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/recommended`);
+  const recommendedProducts = await response.json();
+
+  return {
+   props: {
+      recommendedProducts
+   }
+  }
+}
