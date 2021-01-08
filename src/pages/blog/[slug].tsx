cfe574274b2394b1996a5ced503316b6420af738
patrickcavalcante <patrickc.lima@outlook.com>
2021-01-08 09:13:51 -0300

    rota blog

---
+++ src/pages/blog/[slug].tsx
@@ -0,0 +1,53 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { useRouter } from "next/router";
+import { client } from "../../lib/prismic";
+import PrismicDOM from 'prismic-dom';
+import { Document } from 'prismic-javascript/types/documents';
+
+interface ProductProps {
+  product: Document
+}
+
+export default function Product({ product }: ProductProps) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <p>Carregando...</p>
+  }
+
+  return (
+    <div>
+      <h1>{PrismicDOM.RichText.asText(product.data.title)}</h1>
+      <img src={product.data.thumbnail.url} alt="" width="200 "/>
+      <div dangerouslySetInnerHTML={{ __html: PrismicDOM.RichText.asHtml(product.data.description)}}/>
+      <p>Price:{product.data.price}</p>
+    </div>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+      paths: [],
+      fallback: true
+  }
+};
+
+export const getStaticProps: GetStaticProps<ProductProps> = async (context) => {
+  const { slug } = context.params;
+
+  const product = await client().getByUID('product', String(slug), {});
+
+  if(!product) {
+      return {
+          notFound: true,
+      }
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 10,
+  }
+
+}
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,56 @@
+import { GetServerSideProps } from 'next';
+import { Title } from '../../styles/pages/Home';
+import Link from 'next/link';
+import SEO from '../../components/SEO';
+import { client } from '../../lib/prismic';
+import Prismic from 'prismic-javascript';
+import PrismicDOM from 'prismic-dom';
+import { Document } from 'prismic-javascript/types/documents'
+
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface HomeProps {
+  recommendedProducts: Document[];
+}
+
+export default function Blog({ recommendedProducts }: HomeProps ) {
+  return (
+    <div>
+      <SEO
+       title="DevCommerce, your best e-commerce!"
+       image="boost.png"
+       shouldExcludeTitleSuffix
+      />
+      <section>
+        <Title>Products</Title>
+        <ul>
+          {recommendedProducts.map(recommendedProduct => {
+            return (
+              <li key={recommendedProduct.id}>
+                <Link href={`/catalog/products/${recommendedProduct.uid}`}>
+                  <a>
+                    {PrismicDOM.RichText.asText(recommendedProduct.data.title)}
+                  </a>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<HomeProps> = async () => {
+  const recommendedProducts = await client().query([
+    Prismic.Predicates.at('document.type', 'product')
+  ]);
+  return {
+   props: {
+      recommendedProducts: recommendedProducts.results
+   }
+  }
+}
